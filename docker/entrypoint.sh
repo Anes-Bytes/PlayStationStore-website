c7c0f0f04f8110d8c20e49f78a9d228a16b0aaee
Anes <anesdevpy@gmail.com>
2025-11-25 11:22:40 +0330

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
+set -e
 
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-# Start Django + Gunicorn
-gunicorn config.wsgi:application --bind 0.0.0.0:8000 &
-gunicorn_pid=$!
-
-# Start Telegram Bot
-python manage.py runbot &
-
-wait $gunicorn_pid
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
